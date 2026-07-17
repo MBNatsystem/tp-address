1b9e1c28223499804df870d5dd28ed14875eedda
MBNatsystem <mbiard@natsystem.fr>
2026-07-17 16:10:58 +0200

    Revert "Postgres dvf tarif"

---
+++ src/main/java/fr/natsystem/tp_adresse_test/batch/listener/AddressSkipListener.java
@@ -0,0 +1,27 @@
+package fr.natsystem.tp_adresse_test.batch.listener;
+
+import org.springframework.batch.core.listener.SkipListener;
+import org.springframework.stereotype.Component;
+
+import fr.natsystem.tp_adresse_test.batch.model.RowAddressCsv;
+import lombok.extern.slf4j.Slf4j;
+
+@Component
+@Slf4j
+public class AddressSkipListener implements SkipListener<RowAddressCsv, RowAddressCsv>{
+
+    @Override
+    public void onSkipInRead(Throwable t) {
+        log.warn("Ligne ignorée à la lecture {}", t.getMessage());
+    }
+
+    @Override
+    public void onSkipInProcess(RowAddressCsv item, Throwable t) {
+        log.warn("Élément ignoré pendant le process : {}", item);
+    }
+
+    @Override
+    public void onSkipInWrite(RowAddressCsv item, Throwable t) {
+        log.warn("Élément ignoré pendant l'écriture : {}", item);
+    }
+}
---
+++ src/main/java/fr/natsystem/tp_adresse_test/batch/utils/Constant.java
@@ -0,0 +1,21 @@
+package fr.natsystem.tp_adresse_test.batch.utils;
+
+public class Constant {
+    public static final String DOWNLOADED = "DOWNLOADED";
+
+    public static final String NO_INPUT_FILE = "NO_INPUT_FILE";
+    public static final String MULTIPLE_FILES_FOUND = "MULTIPLE_FILES_FOUND";
+    public static final String INVALID_FILE_FORMAT = "INVALID_FILE_FORMAT";
+
+    public static final String PREPARE_INPPUT_FILE_STEP = "prepareInputFileStep";
+    public static final String CHECK_CSV_FORMAT_STEP = "checkCsvFormatStep";
+    public static final String LOAD_CSV_TO_STAGE_STEP = "loadCsvToStageStep";
+
+    public static final String CHECKSUM = "CHECKSUM";
+
+    public static final String FIRST_LINE = "id;id_fantoir;numero;rep;nom_voie;code_postal;code_insee;nom_commune;code_insee_ancienne_commune;nom_ancienne_commune;x;y;lon;lat;type_position;alias;nom_ld;libelle_acheminement;nom_afnor;source_position;source_nom_voie;certification_commune;cad_parcelles";
+
+    public static final String DOWNLOAD_URL = "DOWNLOAD_URL";
+
+    
+}
