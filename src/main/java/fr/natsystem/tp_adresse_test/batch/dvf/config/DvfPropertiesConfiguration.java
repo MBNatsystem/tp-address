9d79f7b72dce5d8958d9b70782e456874afd5262
Matheo BIARD <mbiar@natsystem.fr>
2026-07-17 16:06:09 +0200

    feature:endpoint tarif from dvf stats

--- src/main/java/fr/natsystem/tp_adresse_test/batch/dvf/config/DvfPropertiesConfiguration.java
+++ src/main/java/fr/natsystem/tp_adresse_test/batch/dvf/config/DvfPropertiesConfiguration.java
@@ -3,6 +3,7 @@ package fr.natsystem.tp_adresse_test.batch.dvf.config;
 import java.nio.file.Path;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.core.io.Resource;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -12,4 +13,5 @@ import lombok.Setter;
 @ConfigurationProperties(prefix = "batch.dvf")
 public class DvfPropertiesConfiguration {
     private Path extractFileName;
+    private Resource vueDvfStatistique;
 }
